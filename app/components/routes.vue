15daac6b6cb79d7854c646224b02c83957aeb613
Anurag <kr.anurag24@gmail.com>
2026-02-28 18:35:09 +0530

    change route name

--- app/components/routes.vue
+++ app/components/routes.vue
@@ -5,8 +5,8 @@ const route = useRoute();
 <template>
   <div class="font-mono text-blue-400 mb-6 flex gap-3 text-base *:hover:text-blue-500">
     <NuxtLink to="/" :class="route.name === 'index' ? 'text-blue-700' : ''">/index</NuxtLink>
-    <NuxtLink to="/clicks" :class="route.name === 'clicks' ? 'text-blue-700' : ''"
-      >/clicks</NuxtLink
+    <NuxtLink to="/photos" :class="route.name === 'photos' ? 'text-blue-700' : ''"
+      >/photos</NuxtLink
     >
   </div>
 </template>
